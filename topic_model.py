89a3bd012802603124af59e068f92e98db595b92
glmack <glmack@gmail.com>
2019-06-24 13:00:43 -0700

    update minor changes to nb pre-processing, and add functions to topic_model.py file

--- topic_model.py
+++ topic_model.py
@@ -1,13 +1,45 @@
 def tokenize_docs(docs):
-    """apply word tokenization to corpus"""
+    """convert words in corpus to word tokens"""
     tokenized_docs = []
     for doc in docs:
         tokenized_docs.append(word_tokenize(doc))
     return tokenized_docs
 
 def clean_docs(tokenized_docs):
+    """clean corpus of punctuation"""
     clean_docs = []
     for doc in tokenized_docs:
        clean_docs.append([word for word in doc if word.isalpha()]) 
     return clean_docs
+
+def lower_words(docs):
+    """convert words in corpusto lowercase"""
+    lowered_words = []
+    for doc in docs:
+        lowered_words.append([word.lower() for word in doc])
+    return lowered_words
+
+def remove_stopwords(clean_docs):
+    """remove standard stopwords from corpus"""
+    filtered_docs = []
+    for doc in clean_docs:
+       filtered_docs.append([word for word in doc if word not in stop_words])
+    return filtered_docs
+
+def bigrams(docs):
+    """create bigrams from corpus"""
+    return [bigram_model[doc] for doc in docs]
+
+def trigrams(docs):
+    """create trigrams from corpus"""
+    return [trigram_model[bigram_model[doc]] for doc in docs]
+
+def lemmatize_docs(docs, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """lemmatize documents"""
+    lemmatized_docs = []
+    for doc in docs: 
+        lemmatized_docs.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return lemmatized_docs
+
+
 
