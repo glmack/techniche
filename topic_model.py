bbc660969570b93e1ea182ccf5d0e9f13128774e
glmack <glmack@gmail.com>
2019-06-24 06:53:01 -0700

    updates nb and .py file with word pre-processing tasks

--- topic_model.py
+++ topic_model.py
@@ -1,5 +1,13 @@
-def tokenize_docs(documents):
-    """apply work tokenization to corpus of documents"""
-    tokenized_docs = documents.map(word_tokenize)
+def tokenize_docs(docs):
+    """apply word tokenization to corpus"""
+    tokenized_docs = []
+    for doc in docs:
+        tokenized_docs.append(word_tokenize(doc))
     return tokenized_docs
+
+def clean_docs(tokenized_docs):
+    clean_docs = []
+    for doc in tokenized_docs:
+       clean_docs.append([word for word in doc if word.isalpha()]) 
+    return clean_docs
 
