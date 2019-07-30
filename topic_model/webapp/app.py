21eba935d96a050ed3b065f160416a5c5eb3be8d
glmack <glmack@gmail.com>
2019-07-30 16:19:06 -0700

    update with initial code using flask brython example

--- topic_model/webapp/app.py
+++ topic_model/webapp/app.py
@@ -1,1 +1,23 @@
-# webapp for deployment of topic_model
+import random
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+import pickle
+from flask import Flask, request, render_template, jsonify
+
+with open('/Users/lee/Documents/techniche/techniche/data/model_lda_1.pkl', 'rb') as f:
+    model = pickle.load(f)
+app = Flask(__name__, static_url_path="")
+
+@app.route('/')
+def index():
+    """Return the main page."""
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    """Return a random prediction."""
+    data = request.json
+    prediction = model.predict_proba([data['user_input']])
+    return jsonify({'probability': prediction[0][1]})
