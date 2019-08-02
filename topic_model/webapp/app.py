de1f62cabababf1ab4751f30713aca870f97987a
glmack <glmack@gmail.com>
2019-08-02 13:21:45 -0700

    updates webapp with templates dir and files from bootstrap

--- topic_model/webapp/app.py
+++ topic_model/webapp/app.py
@@ -1,23 +1,22 @@
 from flask import Flask, request, render_template, jsonify
+import pickle
 
 with open('/Users/lee/Documents/techniche/techniche/data/model_lda_1.pkl', 'rb') as f:
     model = pickle.load(f)
 
-app = Flask(__name__, static_url_path="/www.techniche.app")
+app = Flask(__name__, static_url_path="")
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
 
 @app.route('/')
-def hello():
-    return "hello world"
-# def index():
-  #  """Return the main page."""
-# return render_template('index.html')
+def index():
+    """Return main page"""
+    return render_template('index.html')
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    """Return a random prediction."""
+    """Return prediction"""
     data = request.json
     prediction = model.predict_proba([data['user_input']])
     return jsonify({'probability': prediction[0][1]})
