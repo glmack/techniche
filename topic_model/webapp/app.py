1fbe2a8d0cf5bda522463e5724231ba89d31092e
glmack <glmack@gmail.com>
2019-08-02 17:25:57 -0700

    update app.py and index.html with brython components

--- topic_model/webapp/app.py
+++ topic_model/webapp/app.py
@@ -6,8 +6,8 @@ with open('/Users/lee/Documents/techniche/techniche/data/model_lda_1.pkl', 'rb')
 
 app = Flask(__name__, static_url_path="")
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(port=5000, debug=True)
 
 @app.route('/')
 def index():
@@ -20,3 +20,14 @@ def predict():
     data = request.json
     prediction = model.predict_proba([data['user_input']])
     return jsonify({'probability': prediction[0][1]})
+
+@app.route("/output", methods=["GET", "POST"])
+def output():
+    """Return text from user input"""
+    data = request.get_json(force=True)
+    # every time the user_input identifier
+    print(data)
+    # cast dk to int
+    data["int"] = int(data["dk"])
+    data["mult"] = 10 * data["int"]
+    return jsonify(data["mult"])
