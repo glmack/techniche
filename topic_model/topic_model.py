da104e75698896fde8f37d7ab4eb8dc99542aa15
glmack <glmack@gmail.com>
2019-08-07 09:57:45 -0700

    replace content of topic_model.py with content of test_model.py - major edit

--- topic_model/topic_model.py
+++ topic_model/topic_model.py
@@ -1,4 +1,109 @@
 
+def get_patent_fields_list():
+    """scrape patent fields that are retrievable from PatentsView API"""
+    import requests
+    from bs4 import Tag, NavigableString, BeautifulSoup
+    import pandas as pd
+    url = "http://www.patentsview.org/api/patent.html"
+    page = requests.get(url)
+    table = []
+    table_fieldnames = []
+    soup = BeautifulSoup(page.text, 'lxml')
+    table = soup.find(class_='table table-striped documentation-fieldlist')
+    table_rows = table.find_all('tr')
+    counter = 0
+    for tr in table_rows:
+        if counter == 0:
+            th = tr.find_all('th')
+            row = [tr.text for tr in th]
+            table.append(row)
+            counter += 1
+
+        elif counter > 0:
+            td = tr.find_all('td')
+            row = [tr.text for tr in td]
+            table.append(row)
+
+    for row in l[1:]:
+        table_fieldnames.append(row[0])
+    return table_fieldnames
+
+
+def get_patent_fields_df():
+    """return df from scraped patent fields that are
+     retrievable from PatentsView API"""
+    import requests
+    from bs4 import Tag, NavigableString, BeautifulSoup
+    import pandas as pd
+    url = "http://www.patentsview.org/api/patent.html"
+    page = requests.get(url)
+    table = []
+
+    soup = BeautifulSoup(page.text, 'lxml')
+    table = soup.find(class_='table table-striped documentation-fieldlist')
+    table_rows = table.find_all('tr')
+    counter = 0
+    for tr in table_rows:
+        if counter == 0:
+            th = tr.find_all('th')
+            row = [tr.text for tr in th]
+            table.append(row)
+            counter += 1
+
+        elif counter > 0:
+            td = tr.find_all('td')
+            row = [tr.text for tr in td]
+            table.append(row)
+
+    df = pd.DataFrame(table)
+    return df
+
+
+def get_ml_patents():
+    """builds api query for initial ml dataset"""
+    import json
+    import requests
+    endpoint_url = 'http://www.patentsview.org/api/patents/query'
+    query = {"_or": [{"_text_phrase": {"patent_title": "natural language"}},
+                     {"_text_phrase": {"patent_abstract": "natural language"}},
+                     {"_text_phrase": {"patent_abstract": "machine learning"}},
+                     {"_text_phrase": {"patent_title": "machine learning"}},
+                     {"_text_phrase": {"patent_abstract": "computer vision"}},
+                     {"_text_phrase": {"patent_abstract": "computer vision"}}
+                     ]
+             }
+    # uncomment to use alternate query options
+    # query={"cpc_subgroup_id":"G06T3/4046"}
+    # query = {"_and":[{"_gte":
+    #                   {"patent_date":"2017-01-01"}},
+    #                   {"_lte":{"patent_date":"2017-01-31"}}]}
+    # query={"_and":
+    #         [{"_or":
+    #             [{"_text_phrase":{"patent_title":"machine learning"}}
+    #             ,{"_text_phrase":{"patent_abstract":"machine learning"}}]}
+    #         ,{"_and":
+    #       [{"patent_year":2016}]}]}
+    pat_fields = get_patent_fields_list()
+    fields = pat_fields
+    options = {"per_page": 50}
+    sort = [{"patent_date": "desc"}]
+
+    params = {'q': json.dumps(query),
+              'f': json.dumps(fields),
+              'o': json.dumps(options),
+              's': json.dumps(sort)}
+
+    # request and results
+    response = requests.get(endpoint_url, params=params)
+    status = response.status_code
+    results = response.json()
+    print(results)
+    count = results.get("count")
+    data_resp = results['patents']
+    total_pats = results.get("total_patent_count")
+    return data_resp
+
+
 def get_patents_by_month(begin_date, end_date, pats_per_page):
     """requests patent data from PatentsView API by date range"""
     import json
@@ -8,164 +113,82 @@ def get_patents_by_month(begin_date, end_date, pats_per_page):
     data = []
     results = {}
     count = 1
-    pat_fields = ['assignee_city',
-                  'assignee_country',
-                  'assignee_county',
-                  'assignee_county_fips',
-                  'assignee_first_name',
-                  'assignee_first_seen_date',
-                  'assignee_id',
-                  'assignee_last_name',
-                  'assignee_last_seen_date',
-                  'assignee_lastknown_city',
-                  'assignee_lastknown_country',
-                  'assignee_lastknown_latitude',
-                  'assignee_lastknown_location_id',
-                  'assignee_lastknown_longitude',
-                  'assignee_lastknown_state',
-                  'assignee_latitude',
-                  'assignee_location_id',
-                  'assignee_longitude',
-                  'assignee_organization',
-                  'assignee_sequence',
-                  'assignee_state',
-                  'assignee_state_fips',
-                  'assignee_total_num_inventors',
-                  'assignee_total_num_patents',
-                  'assignee_type',
-                  'cpc_category',
-                  'cpc_first_seen_date',
-                  'cpc_group_id',
-                  'cpc_group_title',
-                  'cpc_last_seen_date',
-                  'cpc_section_id',
-                  'cpc_sequence',
-                  'cpc_subgroup_id',
-                  'cpc_subgroup_title',
-                  'cpc_subsection_id',
-                  'cpc_subsection_title',
-                  'cpc_total_num_assignees',
-                  'cpc_total_num_inventors',
-                  'cpc_total_num_patents',
-                  'detail_desc_length',
-                  'forprior_country',
-                  'forprior_date',
-                  'forprior_docnumber',
-                  'forprior_kind',
-                  'forprior_sequence',
-                  'inventor_city',
-                  'inventor_country',
-                  'inventor_county',
-                  'inventor_county_fips',
-                  'inventor_first_name',
-                  'inventor_first_seen_date',
-                  'inventor_id',
-                  'inventor_last_name',
-                  'inventor_last_seen_date',
-                  'inventor_lastknown_city',
-                  'inventor_lastknown_country',
-                  'inventor_lastknown_latitude',
-                  'inventor_lastknown_location_id',
-                  'inventor_lastknown_longitude',
-                  'inventor_lastknown_state',
-                  'inventor_latitude',
-                  'inventor_location_id',
-                  'inventor_longitude',
-                  'inventor_sequence',
-                  'inventor_state',
-                  'inventor_state_fips',
-                  'inventor_total_num_patents',
-                  'lawyer_first_name',
-                  'lawyer_first_seen_date',
-                  'lawyer_id',
-                  'lawyer_last_name',
-                  'lawyer_last_seen_date',
-                  'lawyer_organization',
-                  'lawyer_sequence',
-                  'lawyer_total_num_assignees',
-                  'lawyer_total_num_inventors',
-                  'lawyer_total_num_patents',
-                  'nber_category_id',
-                  'nber_category_title',
-                  'nber_first_seen_date',
-                  'nber_last_seen_date',
-                  'nber_subcategory_id',
-                  'nber_subcategory_title',
-                  'nber_total_num_assignees',
-                  'nber_total_num_inventors',
-                  'nber_total_num_patents',
-                  'patent_abstract',
-                  'patent_date',
-                  'patent_firstnamed_assignee_city',
-                  'patent_firstnamed_assignee_country',
-                  'patent_firstnamed_assignee_id',
-                  'patent_firstnamed_assignee_latitude',
-                  'patent_firstnamed_assignee_location_id',
-                  'patent_firstnamed_assignee_longitude',
-                  'patent_firstnamed_assignee_state',
-                  'patent_firstnamed_inventor_city',
-                  'patent_firstnamed_inventor_country',
-                  'patent_firstnamed_inventor_id',
-                  'patent_firstnamed_inventor_latitude',
-                  'patent_firstnamed_inventor_location_id',
-                  'patent_firstnamed_inventor_longitude',
-                  'patent_firstnamed_inventor_state',
-                  'patent_kind',
-                  'patent_number',
-                  'patent_processing_time',
-                  'patent_title',
-                  'patent_type',
-                  'patent_year',
-                  'pct_102_date',
-                  'pct_371_date',
-                  'pct_date',
-                  'pct_docnumber',
-                  'pct_doctype',
-                  'pct_kind',
-                  'rawinventor_first_name',
-                  'rawinventor_last_name',
-                  'wipo_field_id',
-                  'wipo_field_title',
-                  'wipo_sector_title',
-                  'wipo_sequence']
-    while True:  # TODO (Lee) - replace with datetime for date range
+    pat_fields = get_patent_fields_list()
+
+    query = {"_and": [{"_gte": {"patent_date": begin_date}},
+             {"_lte": {end_date: "2019-01-01"}}]}
+
+    fields = pat_fields
+    options = {"page": page_counter, "per_page": pats_per_page}
+    sort = [{"patent_date": "desc"}]
+    params = {'q': json.dumps(query),
+              'f': json.dumps(fields),
+              'o': json.dumps(options),
+              's': json.dumps(sort)
+              }
+
+    # response = requests.get(endpoint_url, params = params)
+    # results =  json.loads(response.content)
+    # places.extend(results['results'])
+    # time.sleep(2)
+    # request and results
+    response = requests.get(endpoint_url, params=params)
+    status = response.status_code
+    # print("status:",
+    #        status,
+    #        ';',
+    #        "page_counter:",
+    #        page_counter,)
+    #        ";", "iteration:",i
+    results = response.json()
+    # extract data from response
+    data_response = results['patents']
+    count = results.get("count")
+    total_pats = results.get("total_patent_count")
+    # print("patents on current page:",count,';', "total patents:",total_pats)
+    data.append(data_response)
+
+    # TODO (Lee) - replace with datetime for begin_date to end_date
+    while (total_pats/pats_per_page*page_counter):
         if count == 0:
             print("error/complete")
             break
+
         elif count > 0:
             # build query
-            query = ({"_and":
-                     [{"_gte": {"patent_date": begin_date}},
-                      {"_lte": {end_date: "2019-01-01"}}]},
-                     query={"_or": [{"_text_phrase":
-                            {"patent_title": "natural language"}},
-                            {"_text_phrase":
-                             {"patent_abstract": "natural language"}}]})
-            # {"_or":[{"cpc_subgroup_id": "G06T3/4046"},
-            # {"cpc_subgroup_id": "G06T9/002"}]}
+            query = {"_and": [{"_gte": {"patent_date": begin_date}},
+                              {"_lte": {end_date: "2019-01-01"}}]}
+
+#             {"_or":[{"cpc_subgroup_id": "G06T3/4046"},
+#                     {"cpc_subgroup_id": "G06T9/002"}]}
             fields = pat_fields
             options = {"page": page_counter, "per_page": pats_per_page}
             sort = [{"patent_date": "desc"}]
             params = {'q': json.dumps(query),
                       'f': json.dumps(fields),
                       'o': json.dumps(options),
-                      s's': json.dumps(sort)
+                      's': json.dumps(sort)
                       }
 
             # request and results
-            response = requests.get(endpoint_url, params=params)
-            status = response.status_code
-            # ";", "iteration:", i
-            print("status:", status, ';', "page_counter:", page_counter,)
-            results = response.json()
+            # response = requests.get(endpoint_url, params=params)
+            # status = response.status_code
+            # print("status:", status,
+            #       ';',
+            #       "page_counter:",
+            #       page_counter,)
+            # ";", "iteration:",i
+            # results = response.json()
             # extract data from response
-            data_response = results['patents']
+            # data_response = results['patents']
             count = results.get("count")
-            total_pats = results.get("total_patent_count")
-            print("patents on current page:",
-                  count, ';',
-                  "total patents:", total_pats)
-            data.append(data_response)
+            # total_pats = results.get("total_patent_count")
+            # print("patents on current page:",
+            #       count,
+            #       ';',
+            #       "total patents:",
+            #       total_pats)
+            data.extend(data_response)
             page_counter += 1
 
         else:
@@ -178,8 +201,85 @@ def get_patents_by_month(begin_date, end_date, pats_per_page):
     # TODO (Lee) ? time.sleep(2)
 
 
+def trim_data(data, keys):
+    """subset fields returned from api response"""
+    new_data = []
+    for dictionary in data:
+        new_data.append(
+            dict((k, dictionary[k]) for k in keys if k in dictionary))
+    return new_data
+
+
+def create_title_abstract_col(data):
+    """creates new col from title and abstract cols of api response"""
+    for dictionary in data:
+        dictionary['patent_title_abstract'] =
+        str([dictionary['patent_title'] +
+            '. ' + dictionary['patent_abstract']][0])
+    return data
+
+
+def structure_dataframe(data):
+    """ creates dataframe and organizes columns from dictionary"""
+    import pandas as pd
+    df = pd.DataFrame(data)
+    df = df[['patent_number', 'patent_date', 'patent_title_abstract']]
+    df.sort_values(by=['patent_date'], ascending=True, inplace=True)
+    return df
+
+
+def partition_dataframe(dataframe, train_pct):
+    len(dataframe)
+    text_train = dataframe[:round(len(dataframe)*train_pct)]
+    text_test = dataframe[round(len(dataframe)*train_pct):]
+    return text_train, text_test
+
+
+def build_pipeline():
+    import spacy
+    nlp = spacy.load('en_core_web_sm')
+    # nlp.add_pipe(token_stats, name="token_stats", first=True)
+    # nlp.add_pipe(tokenize, name="tokenize", first=True)
+    return nlp
+
+
+def token_stats(doc):
+    print("After tokenization, this corpus has {} tokens.".format(len(doc)))
+    print("The part-of-speech tags are:", [token.pos_ for token in doc])
+    return doc
+
+
+def process_docs(text_data):
+    import spacy
+    nlp = spacy.load('en_core_web_sm')
+    from nltk.corpus import stopwords
+    """pre-processes patent documents in pipeline"""
+    nlp = build_pipeline()
+    processed_docs = []
+    stop_words = stopwords.words(
+        '/Users/lee/Documents/techniche/techniche/data/stopwords/english')
+    for doc in nlp.pipe(text_data, batch_size=100):
+
+        # ents = doc.ents  # Named entities.
+
+        # Keep only words (no numbers, no punctuation).
+        # Lemmatize tokens, remove punctuation and remove stopwords.
+        doc = [token.text for token in doc if token.text.isalpha()]
+        # [token.text for token in doc]
+        doc = [token.lower() for token in doc]
+        # filter stopwords from nltk list of common stopwords
+        doc = [token for token in doc if token not in stop_words]
+
+        # Add named entities, but only if compound of more than word.
+        # doc.extend([str(entity) for entity in ents if len(entity) > 1])
+
+        processed_docs.append(doc)
+
+    return processed_docs
+
+
 def tokenize_docs(docs):
-    """converts words in corpus to word tokens"""
+    """convert words in corpus to word tokens"""
     tokenized_docs = []
     for doc in docs:
         tokenized_docs.append(word_tokenize(doc))
@@ -187,7 +287,7 @@ def tokenize_docs(docs):
 
 
 def clean_docs(tokenized_docs):
-    """cleans corpus of punctuation"""
+    """clean corpus of punctuation"""
     clean_docs = []
     for doc in tokenized_docs:
         clean_docs.append([word for word in doc if word.isalpha()])
@@ -195,7 +295,7 @@ def clean_docs(tokenized_docs):
 
 
 def lower_words(docs):
-    """converts words in corpus to lowercase"""
+    """convert words in corpusto lowercase"""
     lowered_words = []
     for doc in docs:
         lowered_words.append([word.lower() for word in doc])
@@ -203,25 +303,25 @@ def lower_words(docs):
 
 
 def remove_stopwords(clean_docs):
-    """removes standard stopwords from corpus"""
+    """remove standard stopwords from corpus"""
     filtered_docs = []
     for doc in clean_docs:
         filtered_docs.append([word for word in doc if word not in stop_words])
     return filtered_docs
 
 
-# def make_bigrams(docs):
-#     """creates bigrams from corpus"""
-#     return [bigram_model[doc] for doc in docs]
+def bigrams(docs):
+    """create bigrams from corpus"""
+    return [bigram_model[doc] for doc in docs]
 
 
-# def make_trigrams(docs):
-#     """creates trigrams from corpus"""
-#     return [trigram_model[bigram_model[doc]] for doc in docs]
+def trigrams(docs):
+    """create trigrams from corpus"""
+    return [trigram_model[bigram_model[doc]] for doc in docs]
 
 
 def lemmatize_docs(docs, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
-    """lemmatizes documents"""
+    """lemmatize documents"""
     lemmatized_docs = []
     for doc in docs:
         lemmatized_docs.append(
@@ -230,9 +330,27 @@ def lemmatize_docs(docs, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
 
 
 def convert_bytes(num, suffix='B'):
-    """converts file size int from bytes to aggregate units"""
+    """ convert bytes int to int in aggregate units"""
     for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)\n
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+
+def pat_inv_map(data):
+    """builds patent(idx) mapping to list of inventors for a-t model"""
+    pat_inv_dict = {}
+    for patent in data:
+        idx = data.index(patent)
+        inventors = [inventor['inventor_id']
+                     for inventor in patent['inventors']]
+        pat_number = int(patent['patent_number'])
+        pat_inv_dict[idx] = inventors
+    return pat_inv_dict
+
+
+def get_topics(doc, k=5, model_lda=model_lda):
+    topic_id = sorted(model_lda[doc][0], key=lambda x: -x[1])
+    top_k_topics = [x[0] for x in topic_id[:k]]
+    return [(i, model_lda.print_topic(i)) for i in top_k_topics]\n
