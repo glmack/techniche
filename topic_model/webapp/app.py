f1a449e7e806cf71e20ff103a2b413613e4eb40d
glmack <glmack@gmail.com>
2019-08-09 17:08:53 -0700

    restructure and revise directories and files in topic_model dir
    1) deletes previous bootstrap files located in templates dir
    
    2) revises index.html per c nuno example
    
    3) adds additional html example per ME in templates dir
    
    4) restructures and revises files in static dir

--- topic_model/webapp/app.py
+++ topic_model/webapp/app.py
@@ -1,33 +1,63 @@
+# c nuno example
 from flask import Flask, request, render_template, jsonify
-import pickle
-
-with open('/Users/lee/Documents/techniche/techniche/data/model_lda_1.pkl', 'rb') as f:
-    model = pickle.load(f)
 
 app = Flask(__name__, static_url_path="")
 
-# if __name__ == '__main__':
-#     app.run(port=5000, debug=True)
-
-@app.route('/')
+@app.route("/")
 def index():
-    """Return main page"""
-    return render_template('index.html')
+    """Return the main page."""
+    return render_template("index.html")
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    """Return prediction"""
-    data = request.json
-    prediction = model.predict_proba([data['user_input']])
-    return jsonify({'probability': prediction[0][1]})
 
 @app.route("/output", methods=["GET", "POST"])
 def output():
-    """Return text from user input"""
+    """Retun text from user input"""
     data = request.get_json(force=True)
     # every time the user_input identifier
     print(data)
-    # cast dk to int
-    data["int"] = int(data["dk"])
-    data["mult"] = 10 * data["int"]
-    return jsonify(data["mult"])
+    return jsonify(data["user_input"])
+
+# glm extras
+# import pickle
+# from gensim.corpora import Dictionary
+# # from topic_model import get_topics
+
+# with open('/Users/lee/Documents/techniche/techniche/data/model_4.pkl', 'rb') as f:
+#     model = pickle.load(f)
+
+# app = Flask(__name__, static_url_path="")
+
+# # if __name__ == '__main__':
+# #     app.run(port=5000, debug=True)
+
+# @app.route('/')
+# def index():
+#     """Return main page"""
+#     return render_template('index.html')
+
+# @app.route('/predict', methods=['GET', 'POST'])
+# def predict():
+#     """Return prediction"""
+#     data = request.json
+#     prediction = get_topics(id_to_word_1000train.doc2bow(text_input_1[data['user_input']]), model, k=15)
+#     # return jsonify({'probability': prediction[0][1]})
+#     return jsonify({'probability': prediction})
+
+# @app.route("/output", methods=["GET", "POST"])
+# def output():
+#     """Return text from user input"""
+#     data = request.get_json(force=True)
+#     # every time the user_input identifier
+#     print(data)
+#     # cast dk to int
+#     data["int"] = int(data["dk"])
+#     data["mult"] = 10 * data["int"]
+#     return jsonify(data["mult"])
+
+# # below is direct from c nuno example
+# # def output():
+# #     """Retun text from user input"""
+# #     data = request.get_json(force=True)
+# #     # every time the user_input identifier
+# #     print(data)
+# #     return jsonify(data["user_input"])
