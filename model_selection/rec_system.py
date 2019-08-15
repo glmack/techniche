87a3c9c8afca1d0b994dbda729066d1e4e5695f3
glmack <glmack@gmail.com>
2019-08-14 17:10:43 -0700

    Import math lib in funcs

--- model_selection/rec_system.py
+++ model_selection/rec_system.py
@@ -1,7 +1,9 @@
 def alphanum_to_int(string):
     """encodes alphanumeric string to int"""
+    import math
     return int.from_bytes(string.encode(), 'little')
 
 def int_to_alphanum(string):
     """decodes int to alphaumeric string"""
+    import math
     return n.to_bytes(math.ceil(n.bit_length() / 8), 'little').decode()
