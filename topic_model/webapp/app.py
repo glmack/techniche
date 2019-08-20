87ea8fa04f3a359d7ff2d2a6ea61c43e8f683a73
glmack <glmack@gmail.com>
2019-08-20 11:58:34 -0700

    Lint file and remove extra comments
    Resolve app.py:20:1: E302 expected 2 blank lines, found 1
    
    Resolve app.py:32:1: W293 blank line contains whitespace
    
    Resolve app.py:32:1: W391 blank line at end of file
    
    Resolve app.py:32:42: W292 no newline at end of file
    
    Resolve app.py:32:42: W292 no newline at end of file
    
    Resolve app.py:28:47: W292 no newline at end of file
    
    Remove extra comments from example for prediction func
    
    Edit function comment in first func

--- topic_model/webapp/app.py
+++ topic_model/webapp/app.py
@@ -3,26 +3,26 @@ from gensim.corpora import Dictionary
 from flask import Flask, request, render_template, jsonify
 from topic_model import get_topics
 
-with open('model_4.pkl', 'rb') as f_1:
-    model = pickle.load(f_1)
+with open('model_4.pkl', 'rb') as f:
+    model = pickle.load(f)
 
-with open('id_to_word_1000train.pkl', 'rb') as f_2:
-    id_to_word = pickle.load(f_2)
+with open('id_to_word_1000train.pkl', 'rb') as f:
+    id_to_word = pickle.load(f)
 
 app = Flask(__name__, static_url_path="")
 
+
 @app.route("/")
 def index():
-    """Return the main page."""
+    """Return the main page"""
     return render_template("index.html")
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     """Return a prediction of topics"""
     data = request.json
-    # prediction = model.predict_proba([data['user_input']]) #ME example
-    # print(data)
-    prediction = get_topics(id_to_word.doc2bow([data['user_input']]), model, k=15)
-    # round_prediction = round(prediction[0][1], 2)
+    prediction = get_topics(id_to_word.doc2bow([data['user_input']]),
+                            model,
+                            k=15)
     return jsonify({'prediction': prediction})
-    # return jsonify(data["user_input"]
